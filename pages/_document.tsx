913e0d782ec57719cec53f29cd26a12713232752
Zhuxb <ZhuxbClouds@gmail.com>
2022-12-13 17:44:44 +0800

    feat: 字体适配

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Serif+SC:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
