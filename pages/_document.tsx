dd528c047237fa6d58af3f9165913031de3be615
Zhuxb <ZhuxbClouds@gmail.com>
2023-07-20 17:18:50 +0800

    chore🙈: 修改外链，更换cdn镜像

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,7 @@ class MyDocument extends Document {
           />
           <link
             rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+            href="https://jsd.cdn.zzko.cn/npm/katex@0.16.0/dist/katex.min.css"
             integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
             crossOrigin="anonymous"
           ></link>
