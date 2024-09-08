869ea6f8f788543ac97965934a927e0b753f445f
Zhuxb <ZhuxbClouds@gmail.com>
2024-09-09 04:57:44 +0800

    Refactor footer component to include ICP license information

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,17 @@
-export default function Footer()  {
+export default function Footer() {
   return (
     <footer>
-      <p>© 2024 Zhuxb&apos;s blog</p>
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+        }}
+      >
+        <p>© 2024 Zhuxb&apos;s blog</p>
+        <p>
+          <a href="beian.miit.gov.cn">鄂ICP备2022011304号-2</a>
+        </p>
+      </div>
     </footer>
-  )
+  );
 }
