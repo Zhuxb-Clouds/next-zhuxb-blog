4abf25d1ecb599b7dedcbb22e8f85ead42123b75
Zhuxb-Remilia <ZhuxbClouds@gmail.com>
2026-02-02 11:08:39 +0800

    Delete obsolete posts related to JWT, LLM Web applications, CSS triangle creation, AntD PDF generation, WebGIS libraries, Promise API, and Baidu Map controls to streamline content and improve relevance.

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,33 +12,31 @@ import DarkModeSwitch from "../components/Header/DarkModeSwitch";
 // instrumentation.ts
 import { generateFeedXML } from "../utils/feed";
 
-const TheRoc = () => (<a
-  href="https://hrsrive.cn/game/the-roc"
-  className={style.projectCard}
-  target="__blank"
->
-  <div className={style.imageContainer}>
-    <img
-      src="https://oss.hrsrive.cn/hrsrive/cg06_0302.png"
-      style={{
-        transformOrigin: "53% 27%",
-        transform: "scale(3)",
-      }}
-      alt=""
-    />
-  </div>
-  <div className={style.projectInfo}>
-    <div className={style.projectCardTitle}>大鹏 - The Roc</div>
-
-    <div className={style.projectCardDescription}>
-      <p style={{ margin: 0 }}>A story about novel,freedom and her.</p>
+const TheRoc = () => (
+  <a href="https://hrsrive.cn/game/the-roc" className={style.projectCard} target="__blank">
+    <div className={style.imageContainer}>
+      <img
+        src="https://oss.hrsrive.cn/hrsrive/cg06_0302.png"
+        style={{
+          transformOrigin: "53% 27%",
+          transform: "scale(3)",
+        }}
+        alt=""
+      />
     </div>
-  </div>
-  <div className={style.projectHideTitle}>
-    <span>大</span>
-    <span>鹏</span>
-  </div>
-</a>)
+    <div className={style.projectInfo}>
+      <div className={style.projectCardTitle}>大鹏 - The Roc</div>
+
+      <div className={style.projectCardDescription}>
+        <p style={{ margin: 0 }}>A story about novel,freedom and her.</p>
+      </div>
+    </div>
+    <div className={style.projectHideTitle}>
+      <span>大</span>
+      <span>鹏</span>
+    </div>
+  </a>
+);
 
 const homePage: NextPage<postsData> = ({ postsData }) => {
   return (
@@ -52,7 +50,7 @@ const homePage: NextPage<postsData> = ({ postsData }) => {
             Hi, I&apos;m Zhuxb. <DarkModeSwitch />
           </span>
 
-          <p>Full Stack developer / Writer / Indie Game producer.</p>
+          <p>Full-Stack Developer, Indie Game Creator & Writer.</p>
           <p>Share Everything I know.</p>
           <div style={{ marginBlock: "10px", display: "flex", gap: "10px" }}>
             <a target="view_window" href="https://github.com/Zhuxb-Clouds">
@@ -71,9 +69,7 @@ const homePage: NextPage<postsData> = ({ postsData }) => {
         </div>
         <div className={style.posts}>
           <p className={style.sectionTitle}>Project</p>
-          <div className={style.projectContainer}>
-            {TheRoc()}
-          </div>
+          <div className={style.projectContainer}>{TheRoc()}</div>
         </div>
         <div className={style.posts}>
           <p className={style.sectionTitle}>
